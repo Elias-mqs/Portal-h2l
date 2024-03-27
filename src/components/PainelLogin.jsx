a034d990481cbe2f1a71fb66d74bcf13073b4aaf
Elias-mqs <eliasmqs397@gmail.com>
2024-03-27 14:44:35 -0400

    27-03 login testado

--- src/components/PainelLogin.jsx
+++ src/components/PainelLogin.jsx
@@ -10,41 +10,72 @@ import {
     FormControl,
     useToast,
     Input,
+
+    useDisclosure,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalCloseButton,
+    ModalBody,
+    FormLabel,
+    ModalFooter,
+    Spinner,
+    InputLeftElement,
+    InputRightElement,
+    InputGroup,
+
+
+
+
 } from "@chakra-ui/react"
 import styles from '../styles/PainelLogin.module.css'
-import Link from "next/link"
 import axios from "axios"
+import { IconLock, IconEyeClosed, IconEye } from '@tabler/icons-react';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 import { useRouter } from 'next/router'
-import { useState } from "react"
+import { useState, useRef } from "react"
 
 
 
 
 export default function PainelLogin() {
 
-
-    const toast = useToast()
-    const router = useRouter()
-    const [formulario, setFormulario] = useState({
+    
+    const [show, setShow] = useState(false) ///// CONTROLE DE ESTADO BUTTON EYES
+    const [isLoading, setIsLoading] = useState(false); ///// CONTROLE DE ESTADO SPINNER LOADING
+    const [formulario, setFormulario] = useState({ ///// CONTROLE DE ESTADO FORMULARIO PARA RECEBER DADOS DO USER
         username: '',
         password: ''
     })
 
+    const toast = useToast() ///// INSTANCIA PARA FEEDBACKS AO TENTAR LOGAR (CARDS SUCCESS/ERRROR)
+    const router = useRouter() ///// INSTANCIA PARA REDIRECIONAMENTO
+
+    const handleClickEyes = (e) => { ///// CONTROLE DO EYE PARA NÃO SUBMIT AO MUDAR O ESTADO
+        e.preventDefault(); //EVITA O SUBMIT AO CLICAR
+        setShow(!show);
+    };
 
 
-
+/////////////////// CONST RESPONSAVEL POR MANIPULAR O EVENTO SUBMIT
     const handleLogin = async (e) => {
         e.preventDefault()
+        setIsLoading(true); // INICIA O SPINNER ANTES DA VALIDAÇÃO
+
+        if (isLoading) { // QUANTO ATIVO EVITA QUE O USUARIO EVIE O FORM NOVAMENTE ENQUANTO LOGIN EM ANDAMENTO
+            return;
+        }
+
         try {
             const result = await axios.post('/api/login', formulario)
             const token = result?.data?.token;
 
             localStorage.setItem('token', token);
-
             setFormulario({ username: ``, password: `` })
 
             toast({
-                title: "success",
+                title: "Success",
                 description: result?.data?.message,
                 status: 'success',
                 duration: 2000,
@@ -53,20 +84,17 @@ export default function PainelLogin() {
 
             router.push('/users');
 
-            
 
         } catch (error) {
-            console.log(error)
-
             toast({
-                title: "error",
+                title: "Error",
                 description: error?.response?.data?.message,
                 status: 'error',
                 duration: 2000,
                 isClosable: true,
             })
-
-
+        } finally {
+            setIsLoading(false);
         }
     }
 
@@ -77,13 +105,27 @@ export default function PainelLogin() {
 
         // Atualiza o valor correspondente ao nome do campo do evento
         novosDados[e.target.name] = e.target.value
-        if(e.target.name == 'username') {
+        if (e.target.name == 'username') {
             novosDados.username = e.target.value.toLowerCase()
         }
 
         // Define o novo estado com os dados atualizados
         setFormulario(novosDados);
     }
+
+
+
+
+    ////////////// TESTANDO MODAL DE RECUPERAÇÃO DE SENHA //////////////
+
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const initialRef = useRef();
+
+    const handleInputPass = (e) => {
+
+    }
+
+    ////////////// TESTANDO MODAL DE RECUPERAÇÃO DE SENHA //////////////
 
 
 
@@ -104,33 +146,50 @@ export default function PainelLogin() {
             />
 
             <form onSubmit={handleLogin}>
-                <FormControl top='30%' >
+                <FormControl top='40%' >
                     <Stack spacing='35px' top='30px' left='12.5%'>
                         <Stack spacing='30px'>
                             <Box >
                                 <Text color='#003366' p='0 3px' fontSize={14} fontWeight={500}>Username</Text>
-                                <Input
-                                    variant='flushed'
-                                    placeholder='Username'
-                                    name='username'
-                                    value={formulario.username}
-                                    onChange={handleInputChange}
+                                <InputGroup>
+                                    <InputLeftElement >
+                                        <PersonOutlineOutlinedIcon sx={{ color: `#003366` }} />
+                                    </InputLeftElement>
+                                    <Input
+                                        variant='flushed'
+                                        placeholder='Username'
+                                        name='username'
+                                        value={formulario.username}
+                                        onChange={handleInputChange}
 
-                                />
-
+                                    />
+                                </InputGroup>
                             </Box>
+
                             <Box >
                                 <Text color='#003366' p='0 3px' fontSize={14} fontWeight={500}>Password</Text>
-                                <Input
-                                    type='password'
-                                    name='password'
-                                    variant='flushed'
-                                    placeholder='Password'
-                                    value={formulario.password}
-                                    onChange={handleInputChange}
-                                />
+                                <InputGroup>
+                                    <InputLeftElement>
+                                        <IconLock color='#003366' />
+                                    </InputLeftElement>
+                                    <Input
+                                        type={show ? 'text' : 'password'}
+                                        name='password'
+                                        variant='flushed'
+                                        placeholder='Password'
+                                        value={formulario.password}
+                                        onChange={handleInputChange}
+                                    />
+                                    <InputRightElement>
+                                        <Box as='button' onClick={handleClickEyes}>
+                                            {show ? <IconEye color='#003366' />
+                                                : <IconEyeClosed color='#003366' />}
+                                        </Box>
+                                    </InputRightElement>
+                                </InputGroup>
                             </Box>
                         </Stack>
+
                         <VStack spacing={3}>
                             <Button
                                 type='submit'
@@ -142,27 +201,69 @@ export default function PainelLogin() {
                                 alignItems='center'
                                 justifyContent='center'
                                 fontWeight='550'
-
+                                _hover={{ bg: `#5c7da6`, color: `#FFF` }}
+                                disabled={isLoading}
                             >
-                                Login
+                                {isLoading ? (
+                                    <Spinner
+                                        thickness='3px'
+                                        speed="0.65s"
+                                        color="white"
+                                        size="md"
+                                        position="absolute"
+                                    />
+                                ) : (
+                                    'Login'
+                                )}
                             </Button>
-
-                            <Link href='./recuperarSenha'>
-                                <Button
-                                    bg='transparent'
-                                    fontWeight='500'
-                                    color='#003366'
-                                    fontSize={15}
-                                    cursor='pointer'
-                                    _hover={{ textDecoration: 'underline' }}
-                                >
-                                    Esqueceu a senha?
-                                </Button>
-                            </Link>
                         </VStack>
                     </Stack>
                 </FormControl>
             </form>
+
+
+
+
+
+
+            {/* ////////////// MODAL RECUPERAÇÃO DE SENHA ////////////// */}
+
+
+            <Button color='#003366' top='20%' bg='' _hover={{ bg: `none`, textDecoration: `underline` }} onClick={onOpen}>Esqueceu sua senha?</Button>
+
+            <Modal
+                initialFocusRef={initialRef}
+                isOpen={isOpen}
+                onClose={onClose}
+            >
+                <ModalOverlay />
+                <ModalContent >
+                    <FormControl isRequired>
+                        <ModalHeader>Informe seu e-mail</ModalHeader>
+                        <ModalCloseButton />
+                        <ModalBody pb={6}>
+                            <FormLabel>E-mail</FormLabel>
+                            <Input type='email' placeholder='Digite seu e-mail' />
+                        </ModalBody>
+
+                        <ModalFooter>
+                            <Button colorScheme='blue' mr={3}>
+                                Recuperar senha
+                            </Button>
+                            <Button onClick={onClose}>Cancel</Button>
+                        </ModalFooter>
+                    </FormControl>
+                </ModalContent>
+            </Modal>
+
+
+            {/* ////////////// MODAL RECUPERAÇÃO DE SENHA ////////////// */}
+
+
+
+
+
+
 
         </Flex >
 
