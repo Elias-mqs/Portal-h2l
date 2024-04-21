a2f8527136bfb1355b465538c2f969cf21328984
Elias <eliasmqs397@gmail.com>
2024-04-21 17:51:39 -0400

    atualizando _app

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -12,6 +12,7 @@ export default function App({ Component, pageProps }) {
   const toggleHeader = () => { setIsOpen(!isOpen); };
 
   const { navTabs, activeTab } = NavigationTabs();
+  const xxxl = '1536px';
 
   return (
     <ChakraProvider>
@@ -19,15 +20,17 @@ export default function App({ Component, pageProps }) {
         <Component {...pageProps} />
       ) : (
 
-        <Flex w='100%' h='100%' position={'fixed'} direction='column' bg='#F0F2F5' p={{ base: 0, md: '32px' }} >
-          <Box aria-label='container-header'>
-            <Header isOpen={isOpen} toggleSidebar={toggleHeader} navTabs={navTabs} activeTab={activeTab} />
-          </Box>
-          <Flex aria-label='container-page' justify={'flex-end'} flex='1' sx={{ '&::-webkit-scrollbar': { display: 'none', 'msOverflowStyle': 'none', } }} overflow='auto'  >
-            <Container isOpen={isOpen}>
-              <Component {...pageProps} isOpen={isOpen} />
-            </Container>
-          </Flex>
+        <Flex w='100%' h='100%' position={'fixed'} align='center' direction='column' bg='#F0F2F5' p={{ base: 0, md: '32px' }} >
+          <Stack aria-label='teste' w='100%' maxW={xxxl} h='2000px' align='center' overflow='auto' >
+            <Stack aria-label='container-header' w='100%' >
+              <Header isOpen={isOpen} toggleSidebar={toggleHeader} navTabs={navTabs} activeTab={activeTab} />
+            </Stack>
+            <Flex aria-label='container-page' justify={'flex-end'} flex='1' sx={{ '&::-webkit-scrollbar': { display: 'none', 'msOverflowStyle': 'none', } }} overflow='auto'  >
+              <Container isOpen={isOpen}>
+                <Component {...pageProps} isOpen={isOpen} />
+              </Container>
+            </Flex>
+          </Stack>
         </Flex>
 
       )}
